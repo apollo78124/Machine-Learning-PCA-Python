51a7cba067abbd1d6b8d7f626ba5ed1b11b4995b
apollo78124 <98apollo@naver.com>
2021-04-03 14:25:45 -0700

    Plot digits

--- Dimensionality/main.py
+++ Dimensionality/main.py
@@ -1,16 +1,121 @@
-# This is a sample Python script.
+import inline as inline
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import plotly.offline as py
+#py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.tools as tls
+import plotly.subplots as sbplt
+import seaborn as sns
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import matplotlib
+#%matplotlib inline
 
+# Import the 3 dimensionality reduction methods
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+train = pd.read_csv('./train.csv')
+train.head()
+print(train.shape)
+# save the labels to a Pandas series target
+target = train['label']
+# Drop the label feature
+train = train.drop("label",axis=1)
+# Standardize the data
+from sklearn.preprocessing import StandardScaler
+X = train.values
+X_std = StandardScaler().fit_transform(X)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Calculating Eigenvectors and eigenvalues of Cov matirx
+mean_vec = np.mean(X_std, axis=0)
+cov_mat = np.cov(X_std.T)
+eig_vals, eig_vecs = np.linalg.eig(cov_mat)
+# Create a list of (eigenvalue, eigenvector) tuples
+eig_pairs = [ (np.abs(eig_vals[i]),eig_vecs[:,i]) for i in range(len(eig_vals))]
 
+# Sort the eigenvalue, eigenvector pair from high to low
+eig_pairs.sort(key = lambda x: x[0], reverse= True)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Calculation of Explained Variance from the eigenvalues
+tot = sum(eig_vals)
+var_exp = [(i/tot)*100 for i in sorted(eig_vals, reverse=True)] # Individual explained variance
+cum_var_exp = np.cumsum(var_exp) # Cumulative explained variance
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+trace1 = go.Scatter(
+    x=list(range(784)),
+    y= cum_var_exp,
+    mode='lines+markers',
+    name="'Cumulative Explained Variance'",
+#     hoverinfo= cum_var_exp,
+    line=dict(
+        shape='spline',
+        color = 'goldenrod'
+    )
+)
+trace2 = go.Scatter(
+    x=list(range(784)),
+    y= var_exp,
+    mode='lines+markers',
+    name="'Individual Explained Variance'",
+#     hoverinfo= var_exp,
+    line=dict(
+        shape='linear',
+        color = 'black'
+    )
+)
+fig = sbplt.make_subplots(insets=[{'cell': (1,1), 'l': 0.7, 'b': 0.5}], print_grid=True)
+
+fig.append_trace(trace1, 1, 1)
+fig.append_trace(trace2,1,1)
+fig.layout.title = 'Explained Variance plots - Full and Zoomed-in'
+fig.layout.xaxis = dict(range=[0, 80], title = 'Feature columns')
+fig.layout.yaxis = dict(range=[0, 60], title = 'Explained Variance')
+# fig['data'] = []
+# fig['data'].append(go.Scatter(x= list(range(784)) , y=cum_var_exp, xaxis='x2', yaxis='y2', name = 'Cumulative Explained Variance'))
+# fig['data'].append(go.Scatter(x= list(range(784)) , y=cum_var_exp, xaxis='x2', yaxis='y2', name = 'Cumulative Explained Variance'))
+
+# fig['data'] = go.Scatter(x= list(range(784)) , y=cum_var_exp, xaxis='x2', yaxis='y2', name = 'Cumulative Explained Variance')]
+# fig['data'] += [go.Scatter(x=list(range(784)), y=var_exp, xaxis='x2', yaxis='y2',name = 'Individual Explained Variance')]
+
+# # fig['data'] = data
+# # fig['layout'] = layout
+# # fig['data'] += data2
+# # fig['layout'] += layout2
+# py.iplot(fig, filename='inset example')
+# Invoke SKlearn's PCA method
+n_components = 30
+pca = PCA(n_components=n_components).fit(train.values)
+
+eigenvalues = pca.components_.reshape(n_components, 28, 28)
+
+# Extracting the PCA components ( eignevalues )
+#eigenvalues = pca.components_.reshape(n_components, 28, 28)
+eigenvalues = pca.components_
+n_row = 4
+n_col = 7
+
+# Plot the first 8 eignenvalues
+plt.figure(figsize=(13,12))
+for i in list(range(n_row * n_col)):
+    offset =0
+    plt.subplot(n_row, n_col, i + 1)
+    plt.imshow(eigenvalues[i].reshape(28,28), cmap='jet')
+    title_text = 'Eigenvalue ' + str(i + 1)
+    plt.title(title_text, size=6.5)
+    plt.xticks(())
+    plt.yticks(())
+#plt.show()
+plt.figure(figsize=(14,12))
+for digit_num in range(0,70):
+    plt.subplot(7,10,digit_num+1)
+    #depricated
+    #grid_data = train.iloc[digit_num].as_matrix().reshape(28,28)  # reshape from 1d to 2d pixel array
+    grid_data = train.iloc[digit_num].to_numpy().reshape(28, 28)  # reshape from 1d to 2d pixel array
+    plt.imshow(grid_data, interpolation = "none", cmap = "afmhot")
+    plt.xticks([])
+    plt.yticks([])
+plt.tight_layout()
+plt.show()
